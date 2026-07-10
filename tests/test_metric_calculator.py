3d1861c653461dd1a5adeb1d1a4b7629585dc008
sonina-marina <mar1na.sonina@yandex.ru>
2026-07-10 20:25:42 +0300

    feat: add test for metric-calculator: cac, ltc, roi

--- tests/test_metric_calculator.py
+++ tests/test_metric_calculator.py
@@ -1,3 +1,5 @@
+import pytest
+
 from schemas.metric import ProductCalculationData
 from services.metric_calculator import MetricCalculator
 
@@ -27,3 +29,142 @@ def test_conversion():
     )
 
     assert calculator.calculate_conversion(data) == 0.1
+    
+
+def test_conversion_zero_views():
+
+    calculator = MetricCalculator()
+
+    data = ProductCalculationData(
+        price=1000,
+        cogs=400,
+        commission=10,
+        acquiring=2,
+        tax=6,
+        views=0,
+        target_actions=100,
+        buyers=20,
+        sales=25,
+        ad_costs=5000,
+        inbound_logistic=30,
+        direct_logistic=40,
+        reverse_logistic=50,
+        return_rate=0.1,
+        defect_rate=0.05,
+        avg_storage=15,
+        avg_packaging=10
+    )
+
+    with pytest.raises(ValueError):
+        calculator.calculate_conversion(data)
+
+
+def test_cac():
+
+    calculator = MetricCalculator()
+
+    data = ProductCalculationData(
+        price=1000,
+        cogs=400,
+        commission=10,
+        acquiring=2,
+        tax=6,
+        views=1000,
+        target_actions=100,
+        buyers=20,
+        sales=25,
+        ad_costs=5000,
+        inbound_logistic=30,
+        direct_logistic=40,
+        reverse_logistic=50,
+        return_rate=0.1,
+        defect_rate=0.05,
+        avg_storage=15,
+        avg_packaging=10
+    )
+
+    assert calculator.calculate_cac(data) == 250
+    
+
+def test_cac_zero_byuers():
+
+    calculator = MetricCalculator()
+
+    data = ProductCalculationData(
+        price=1000,
+        cogs=400,
+        commission=10,
+        acquiring=2,
+        tax=6,
+        views=1000,
+        target_actions=100,
+        buyers=0,
+        sales=25,
+        ad_costs=5000,
+        inbound_logistic=30,
+        direct_logistic=40,
+        reverse_logistic=50,
+        return_rate=0.1,
+        defect_rate=0.05,
+        avg_storage=15,
+        avg_packaging=10
+    )
+
+    with pytest.raises(ValueError):
+        calculator.calculate_cac(data)
+
+
+def test_ltc():
+
+    calculator = MetricCalculator()
+
+    data = ProductCalculationData(
+        price=1000,
+        cogs=400,
+        commission=10,
+        acquiring=2,
+        tax=6,
+        views=1000,
+        target_actions=100,
+        buyers=20,
+        sales=25,
+        ad_costs=5000,
+        inbound_logistic=30,
+        direct_logistic=40,
+        reverse_logistic=50,
+        return_rate=0.1,
+        defect_rate=0.05,
+        avg_storage=15,
+        avg_packaging=10
+    )
+
+    assert calculator.calculate_ltc(data) == 682
+    
+
+def test_product_roi():
+
+    calculator = MetricCalculator()
+
+    data = ProductCalculationData(
+        price=1000,
+        cogs=400,
+        commission=10,
+        acquiring=2,
+        tax=6,
+        views=1000,
+        target_actions=100,
+        buyers=0,
+        sales=25,
+        ad_costs=5000,
+        inbound_logistic=30,
+        direct_logistic=40,
+        reverse_logistic=50,
+        return_rate=0.1,
+        defect_rate=0.05,
+        avg_storage=15,
+        avg_packaging=10
+    )
+
+    with pytest.raises(ValueError):
+        calculator.calculate_product_roi(318, 0)
+
