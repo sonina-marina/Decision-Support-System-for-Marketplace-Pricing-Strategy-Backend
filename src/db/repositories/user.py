d3305b9e7226fe870a7d092a7fb7795ca2da78b1
sonina-marina <mar1na.sonina@yandex.ru>
2026-07-17 14:52:10 +0300

    feat(repositories): add method get_user_by_email in UserRepository

--- src/db/repositories/user.py
+++ src/db/repositories/user.py
@@ -1,5 +1,6 @@
 from typing import TypeVar
 
+from pydantic import EmailStr
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.repositories.base import BaseRepository, ModelType
@@ -30,3 +31,18 @@ class UserRepository(
         result = await self.session.execute(stmt)
 
         return result.scalar_one()
+
+
+    async def get_user_by_email(
+        self,
+        email: EmailStr
+    ) -> User | None:
+
+        stmt = (
+            select(User)
+            .where(User.email == email)
+        )
+
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
